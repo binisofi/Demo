b10c59fbe5c76e2ae8012e8054bb50dc0b7d4b11
Biniman <biniuber1@yahoo.com>
2022-07-15 21:01:47 -0400

    first commit

---
+++ src/emp/Employee.java
@@ -0,0 +1,32 @@
+package emp;
+
+import java.util.Scanner;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String name;
+		int age;
+		double salary;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your name");
+		
+		name = input.nextLine();
+		System.out.println("Enter your age");
+		
+		age = input.nextInt();
+		 System.out.println("Enter your Salary");
+		 salary = input.nextDouble();
+		
+		System.out.println("Your name: " +name);
+		System.out.println("Your age: " +age);
+		System.out.println("your salary: " +salary);
+		
+		
+	}
+
+}
